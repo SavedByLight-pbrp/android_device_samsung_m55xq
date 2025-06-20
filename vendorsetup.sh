8c61899ae7460fed2528de9e24347a99e4a3726e
Jamie Falconer <mrfluffyoven@gmail.com>
2025-06-20 10:31:00 +0100

    adapt tree for Orangefox

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Orangefox Flags
+export ALLOW_MISSING_DEPENDANCIES=true
+export TARGET_ARCH=arm64
+export OF_FLASHLIGHT_ENABLE=1
+export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
+export FOX_VERSION="Beta 0.0.1"
+export OF_MAINTAINER="SavedByLight"
+export TARGET_DEVICE_ALT="M55xq"
+export OF_ENABLE_LPTOOLS=1
+export OF_USE_LEGACY_BATTERY_SERVICES=1
+export OF_SCREEN_H=1800
